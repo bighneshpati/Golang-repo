8952b918b1f3cf524d9d3835907a5726a83a4c88
Bighnesh Kumar Pati <bighneshpati@gmail.com>
2022-03-08 00:34:52 +0530

    new commit for the go repo

--- 01hello/main.go
+++ 01hello/main.go
@@ -7,4 +7,15 @@ func main() {
 	if (34 < 36) && (67 < 89) {
 		fmt.Println("Hello to the new world")
 	}
+	for i := 0; i < 3; i++ {
+		fmt.Println("hello world to the for loop")
+	}
+	i := 0
+	for i <= 5 {
+		if i == 3 {
+			break
+		}
+		fmt.Println(i)
+		i += 1
+	}
 }
